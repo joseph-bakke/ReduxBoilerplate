c507867a94e70446f4c265972f538bae5378abda
Joe Bakke <jbakke@cbtnuggets.com>
2016-10-12 16:57:55 -0700

    Remove root component, move initialization into index, add index for actions

--- index.js
+++ index.js
@@ -1,6 +1,31 @@
 import React from 'react';
 import { render } from 'react-dom';
-import Root from './components/Root';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
+import rootReducer from './data/reducers';
+import App from './components/App';
 import './style/main.scss';
 
-render(<Root />, document.getElementById('root'));
+const composeEnhancers = composeWithDevTools({
+    name: 'ReduxBoilerplate',
+});
+
+const initialState = {};
+
+const store = createStore(
+    rootReducer,
+    initialState,
+    composeEnhancers(
+        applyMiddleware(
+            thunkMiddleware
+        )
+    )
+);
+
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+    , document.getElementById('root'));
